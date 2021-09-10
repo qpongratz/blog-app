e298f234924066a8346f31785ed9499ab10b530d
qpongratz <lagarathan@gmail.com>
2021-09-10 17:04:41 -0500

    Fix bug with destroying comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,7 +7,8 @@ class CommentsController < ApplicationController
 
   def destroy
     @article = Article.find(params[:article_id])
-    @comment = @article.comments.find(comment_params)
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy
     redirect_to article_path(@article)
   end
 
